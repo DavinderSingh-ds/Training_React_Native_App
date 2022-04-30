5f2df42979a665b0468a1fcd4551bc232639571d
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-04-30 07:41:52 +0530

    Updating State by TextInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {View,Text,StyleSheet,TextInput} from 'react-native';
 
 const TextScreen = () => {
+    const [name,setName] = useState('');
+
     return (
         <View>
             <TextInput 
                 style = {styles.input}
                 autoCapitalize="none"
-                autoCorrect={false}                    
+                autoCorrect={false}   
+                value={name}   
+                onChangeText={(newValue) => setName(newValue)}              
             />
+            <Text>My Name is : {name}</Text>
         </View>
     );
 };
